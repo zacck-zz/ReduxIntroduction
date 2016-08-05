e6b0b3e160cfd52b48f112801ed1f66cf75c1c12
Zacck <coderv63@gmail.com>
2016-08-05 13:16:54 +0200

    running async actions

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,5 +1,5 @@
 var redux = require('redux');
-
+var axios = require('axios');
 console.log('starting redux');
 
 //Name Reducers and Action Generators
@@ -94,12 +94,59 @@ var removeMovie = (id) => {
   }
 };
 
+//----------------------
+//MAP Reducer
+var mapReducer = (state = {isFetching: false, url: undefined}, action) => {
+  switch(action.type) {
+    case 'START_LOCATION_FETCH':
+      return {
+        isFetching: true,
+        url: undefined
+      };
+    case 'COMPLETE_LOCATION_FETCH':
+      return {
+        isFetching: false,
+        url: action.url
+      };
+    default:
+      return state;
+  }
+};
+
+//Action generator
+var startLocationFetch =  () => {
+  return {
+    type: 'START_LOCATION_FETCH'
+  };
+};
+
+var completeLocationFetch =  (url) => {
+  return {
+    type: 'COMPLETE_LOCATION_FETCH',
+    url
+  };
+};
+
+
+var fetchLocation = () => {
+  store.dispatch(startLocationFetch());
+  //Request data
+  axios.get('http://ipinfo.io').then(function (res) {
+    var loc = res.data.loc;
+    var baseUrl = 'http://maps.google.com?q='
+
+    store.dispatch(completeLocationFetch(baseUrl+loc))
+  }, function(){
+
+  });
+};
 //combineReducer takes one object argument
 //the object contains key value pairs of reducers
 var reducer = redux.combineReducers({
   name: nameReducer,
   hobbies: hobbiesReducer,
-  movies: moviesReducer
+  movies: moviesReducer,
+  map: mapReducer
 });
 var store = redux.createStore(reducer, redux.compose(
   window.devToolsExtension ? window.devToolsExtension() : f => f
@@ -110,11 +157,17 @@ var unsubscribe = store.subscribe(() => {
   var state = store.getState();
 
   console.log('name is ', state.name);
-  document.getElementById('app').innerHTML = state.name;
+  if(state.map.isFetching) {
+    document.getElementById('app').innerHTML = 'Loading...'
+  } else if(state.map.url) {
+    document.getElementById('app').innerHTML = '<a href="'+state.map.url+'" target="_blank">View Your Location</a>'
+  }
   console.log('New State', store.getState());
 });
 
 //unsubscribe();
+
+fetchLocation();
 
 var currentState = store.getState();
 
