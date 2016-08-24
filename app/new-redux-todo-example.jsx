bb74b9d6ca784e17a78d42455a138968e96fc977
Zacck <coderv63@gmail.com>
2016-08-24 10:39:10 +0200

    add actions for adding and removing todos include filter

--- app/new-redux-todo-example.jsx
+++ app/new-redux-todo-example.jsx
@@ -7,22 +7,27 @@ var mState =  {
   showCompleted: 'false',
   todos: [
     {
-      id: 1,
+      id: 19999,
       text: 'go see client',
       createdAt: 1235
     },
     {
-      id: 2,
+      id: 2000,
       text: 'work on redux',
       createdAt: 19799
     },
     {
-      id: 3,
+      id: 30000,
       text: "walk the Dog",
       createdAt: 179739
     }
-  ]
+  ],
+  movies: []
 }
+
+var nextTodoId = 1;
+
+var nextMovieId = 200;
 
 var reducer = (state = mState, action) => {
 
@@ -32,6 +37,38 @@ var reducer = (state = mState, action) => {
       return {
         ...state,
         searchText: action.searchText
+      };
+    case 'ADD_TODO':
+      return {
+        ...state,
+        /*set the todos array equal to current array
+        and add the new todo */
+        todos:[
+          ...state.todos,
+          {
+            text: action.text,
+            id: nextTodoId++
+          }
+        ]
+      };
+    case 'REMOVE_TODO':
+     return {
+       ...state,
+       todos: [
+         ...state.todos.filter((todo) => todo.id !== action.id)
+       ]
+     };
+    case 'ADD_MOVIE':
+      return {
+        ...state,
+        movies: [
+          ...state.movies,
+          {
+            name: action.name,
+            genre: action.genre,
+            id: nextMovieId++
+          }
+        ]
       };
     default:
       //always return state
@@ -67,11 +104,35 @@ store.dispatch(action);
 store.dispatch({
   type: 'CHANGE_SEARCH_TEXT',
   searchText: 'new text'
-})
+});
 
 
 unsubscribe();
 store.dispatch({
   type: 'CHANGE_SEARCH_TEXT',
   searchText: 'new new text'
-})
+});
+
+
+store.dispatch({
+  type: 'ADD_TODO',
+  text: 'Go Buy skate wheels'
+});
+
+store.dispatch({
+  type:'ADD_MOVIE',
+  name: 'Avengers of Ultron',
+  genre: 'Action'
+});
+store.dispatch({
+  type:'ADD_MOVIE',
+  name: 'Marvel Something',
+  genre: 'Action'
+});
+
+store.dispatch({
+  type: 'REMOVE_TODO',
+  id: 30000
+});
+
+console.log('current state after unsubscribe and add todo ', store.getState());
